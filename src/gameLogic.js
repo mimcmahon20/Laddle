be0cfde73522736d8ea4b3a2ccc5fe2433c7f79c
Maguire McMahon <mimcmahon20@vt.edu>
2023-10-29 22:42:53 -0400

    Functioning basic game

---
+++ src/gameLogic.js
@@ -0,0 +1,109 @@
+let selectedLetterIndex = null;
+
+
+function clearHighlights() {
+  document.querySelectorAll(".letter").forEach((button) => {
+    button.style.backgroundColor = "transparent";
+  });
+}
+
+function isOneLetterChanged(word1, word2) {
+  if (word1.length !== word2.length) return false;
+
+  let changes = 0;
+  for (let i = 0; i < word1.length; i++) {
+    if (word1[i] !== word2[i]) changes++;
+    if (changes > 1) return false;
+  }
+  return changes === 1;
+}
+async function isValidWord(word) {
+  try {
+    const response = await fetch(
+      `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+    );
+    const data = await response.json();
+
+    // Check if the response contains a title indicating no definitions found
+    if (data.title && data.title === "No Definitions Found") {
+      return false;
+    }
+
+    // If there's no error title, assume it's a valid word
+    return true;
+  } catch (error) {
+    console.error("Error checking word validity:", error);
+    return false;
+  }
+}
+
+async function getRandomWord() {
+  try {
+    const response = await fetch(
+      "https://random-word-api.herokuapp.com/word?length=4"
+    );
+    const [word] = await response.json(); // API returns an array, we need the first element
+    return word.toUpperCase();
+  } catch (error) {
+    console.error("Error fetching random word:", error);
+    return null;
+  }
+}
+
+async function setRandomWords() {
+  const currentWord = await getRandomWord();
+  const targetWord = await getRandomWord();
+  
+  if (!gameState.currentWord || !gameState.targetWord) {
+    // If it's the first time (or the words are not set), set the current and target words
+    gameState.currentWord = currentWord;
+    gameState.targetWord = targetWord;
+    console.log("currentWord", currentWord);
+    console.log("targetWord", targetWord);
+  } else {
+    // Otherwise, store them for the next turn
+    gameState.nextCurrentWord = currentWord;
+    gameState.nextTargetWord = targetWord;
+    console.log("currentWord", currentWord);
+    console.log("targetWord", targetWord);
+  }
+}
+
+function turnCounter(turns) {
+  return turns + 1;
+}
+
+function checkWinCondition(currentWord, targetWord, turns) {
+  if (currentWord === targetWord) return "win";
+  if (turns >= 10) return "lose"; // you can adjust the max allowed turns
+  return "continue";
+}
+
+const gameState = {
+  currentWord: "FAST",
+  targetWord: "MAKE",
+  turnsTaken: 0,
+  status: "ongoing", // can be 'ongoing', 'win', or 'lose'
+  pathOfWords: [],
+  nextCurrentWord: null,
+  nextTargetWord: null,
+};
+
+function updateWordWithGuess(index, letter) {
+  const currentWordArray = gameState.currentWord.split("");
+  currentWordArray[index] = letter;
+  return currentWordArray.join("");
+}
+
+setRandomWords();
+
+export {
+  gameState,
+  checkWinCondition,
+  isOneLetterChanged,
+  isValidWord,
+  turnCounter,
+  updateWordWithGuess,
+  getRandomWord,
+  setRandomWords
+};
---
+++ src/gameUI.js
@@ -0,0 +1,167 @@
+import {
+  gameState,
+  isOneLetterChanged,
+  isValidWord,
+  turnCounter,
+  checkWinCondition,
+  updateWordWithGuess,
+  getRandomWord,
+  setRandomWords,
+} from "./gameLogic.js";
+
+let selectedLetterIndex = null;
+
+window.addEventListener("DOMContentLoaded", () => {
+  // DOM Elements
+  const letterButtons = document.querySelectorAll(".letter");
+  const submitButton = document.getElementById("submit-guess");
+  const feedbackDiv = document.querySelector(".feedback");
+  const turnsDisplay = document.querySelector(".turns");
+  const newGameButton = document.getElementById("new-game");
+  const darkModeToggle = document.getElementById("dark-mode-toggle");
+  const keys = document.querySelectorAll(".key");
+  let isDarkMode = false;
+  let selectedKey = null;
+
+  keys.forEach((key) => {
+    key.addEventListener("click", (e) => {
+      const guessedLetter = e.target.getAttribute("data-key");
+      if (guessedLetter) {
+        selectedKey = guessedLetter;
+      }
+      if (selectedLetterIndex !== null && guessedLetter) {
+        letterButtons[selectedLetterIndex].textContent = guessedLetter;
+      }
+    });
+  });
+
+  darkModeToggle.addEventListener("click", () => {
+    isDarkMode = !isDarkMode; // Toggle the dark mode flag
+    if (isDarkMode) {
+      document.body.classList.add("dark-mode");
+      darkModeToggle.textContent = "Toggle Light Mode";
+    } else {
+      document.body.classList.remove("dark-mode");
+      darkModeToggle.textContent = "Toggle Dark Mode";
+    }
+  });
+
+  newGameButton.addEventListener("click", async () => {
+    gameState.turnsTaken = 0;
+    gameState.status = "ongoing";
+    gameState.currentWord = gameState.nextCurrentWord;
+    gameState.targetWord = gameState.nextTargetWord;
+    // Update UI
+    updateUI();
+    setRandomWords();
+
+    // Clear feedback if any
+    feedbackDiv.textContent = "";
+  });
+
+  // Highlight the selected letter and store its index
+  letterButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      // Reset all letters to their current state
+      const wordArray = gameState.currentWord.split("");
+      letterButtons.forEach((btn, idx) => {
+        btn.textContent = wordArray[idx];
+        btn.style.backgroundColor = "transparent"; // Reset color
+      });
+
+      // Highlight the newly selected letter
+      e.target.style.backgroundColor = "#e0e0e0";
+      selectedLetterIndex = parseInt(e.target.dataset.index);
+    });
+  });
+
+  submitButton.addEventListener("click", () => {
+    const guessedLetter = selectedKey;
+    console.log(guessedLetter);
+    if (selectedLetterIndex !== null && guessedLetter) {
+      handleGuess(selectedLetterIndex, guessedLetter);
+    }
+  });
+
+  document.addEventListener("keydown", (e) => {
+    const guessedLetter = e.key.toUpperCase();
+
+    if (
+      guessedLetter.length <= 1 &&
+      selectedLetterIndex !== null &&
+      guessedLetter
+    ) {
+      //handleGuess(selectedLetterIndex, guessedLetter);
+      selectedKey = guessedLetter;
+      letterButtons[selectedLetterIndex].textContent = selectedKey;
+    }
+    //try to submit if key is enter
+    if (e.key == "Enter") {
+      console.log("enter pressed");
+      if (selectedLetterIndex !== null && guessedLetter) {
+        console.log(selectedLetterIndex, selectedKey);
+        handleGuess(selectedLetterIndex, selectedKey);
+      }
+    }
+  });
+
+  async function handleGuess(index, letter) {
+    const newWord = updateWordWithGuess(index, letter);
+    // Both checks are now asynchronous
+    if (isOneLetterChanged(gameState.currentWord, newWord)) {
+      if (await isValidWord(newWord)) {
+        gameState.currentWord = newWord;
+        gameState.turnsTaken = turnCounter(gameState.turnsTaken);
+        gameState.status = checkWinCondition(
+          gameState.currentWord,
+          gameState.targetWord,
+          gameState.turnsTaken
+        );
+        letterButtons[selectedLetterIndex].style.backgroundColor = "#4CAF50";
+        updateUI();
+      } else {
+        letterButtons[selectedLetterIndex].style.backgroundColor = "#E54B31";
+        displayFeedback("Invalid guess. Please try again.");
+      }
+    } else {
+      displayFeedback("Invalid guess. Please try again.");
+    }
+  }
+
+  function updateUI() {
+    // Update displayed word
+    const wordArray = gameState.currentWord.split("");
+    letterButtons.forEach((button, index) => {
+      button.textContent = wordArray[index];
+    });
+
+    // Update turns count
+    turnsDisplay.textContent = `Turns: ${gameState.turnsTaken}`;
+
+    // Check win/lose status and provide feedback
+    if (gameState.status === "win") {
+      displayFeedback(
+        "Congratulations! You've transformed the word successfully."
+      );
+    } else if (gameState.status === "lose") {
+      displayFeedback("Sorry, you've exceeded the maximum turns. Try again!");
+    } else {
+      feedbackDiv.textContent = ""; // clear feedback if game is ongoing
+    }
+
+    const targetWordDisplay = document.getElementById("display-target-word");
+    targetWordDisplay.textContent = gameState.targetWord;
+  }
+
+  function clearHighlights() {
+    letterButtons.forEach((button) => {
+      button.style.backgroundColor = "transparent";
+    });
+  }
+
+  function displayFeedback(message) {
+    feedbackDiv.textContent = message;
+  }
+
+  updateUI();
+});

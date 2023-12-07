4500c38f9bffe4aed0928e4b7887d7f84394d7bb
Maguire McMahon <mimcmahon20@vt.edu>
2023-12-06 20:55:18 -0500

    Fixing new data

--- js/generateData.js
+++ js/generateData.js
@@ -1,39 +1,39 @@
-// const fetch = require('node-fetch');
-// const fs = require('fs');
+const fetch = require('node-fetch');
+const fs = require('fs');
 
-// const API_URL = 'https://random-word-api.vercel.app/api?words=1&length=4&type=uppercase';
+const API_URL = 'https://random-word-api.vercel.app/api?words=1&length=4&type=uppercase';
 
-// async function fetchWord() {
-//     const response = await fetch(API_URL);
-//     const data = await response.json();
-//     return data[0];
-// }
+async function fetchWord() {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    return data[0];
+}
 
-// async function generateData() {
-//     const data = [];
-//     let currentDate = new Date(2023, 12, 5); // Jan 5th, 2023
-//     const endDate = new Date(2024, 2, 1);  // Feb 1st, 2024
+async function generateData() {
+    const data = [];
+    let currentDate = new Date(2023, 12, 1); // Jan 5th, 2023
+    const endDate = new Date(2023, 12, 31);  // Feb 1st, 2024
 
-//     while (currentDate <= endDate) {
-//         const startingWord = await fetchWord();
-//         let targetWord = await fetchWord();
+    while (currentDate <= endDate) {
+        const startingWord = await fetchWord();
+        let targetWord = await fetchWord();
         
-//         // Ensure startingWord and targetWord are not the same
-//         while (startingWord === targetWord) {
-//             targetWord = await fetchWord();
-//         }
+        // Ensure startingWord and targetWord are not the same
+        while (startingWord === targetWord) {
+            targetWord = await fetchWord();
+        }
 
-//         data.push({
-//             date: currentDate.toISOString().slice(0, 10),
-//             startingWord,
-//             targetWord
-//         });
+        data.push({
+            date: currentDate.toISOString().slice(0, 10),
+            startingWord,
+            targetWord
+        });
 
-//         currentDate.setDate(currentDate.getDate() + 1); // Move to the next day
-//     }
+        currentDate.setDate(currentDate.getDate() + 1); // Move to the next day
+    }
 
-//     fs.writeFileSync('word_data.json', JSON.stringify(data, null, 4));
-//     console.log('Data saved to word_data.json');
-// }
+    fs.writeFileSync('word_data.json', JSON.stringify(data, null, 4));
+    console.log('Data saved to word_data.json');
+}
 
-// generateData();
+generateData();
